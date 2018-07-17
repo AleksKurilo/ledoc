0330f13aa99827add80fd4fea3483f0b43482b93
Anton Zaporozhchenko <anton.zaporozhchenko@chisw.com>
2018-07-17 18:09:59 +0300

    Add authentication and authorization.

--- src/main/java/dk/ledocsystem/ledoc/config/SecurityConfig.java
+++ src/main/java/dk/ledocsystem/ledoc/config/SecurityConfig.java
@@ -1,27 +1,59 @@
 package dk.ledocsystem.ledoc.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import javax.sql.DataSource;
 import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final String USERS_BY_USERNAME_QUERY = "select username, password, NOT archived " +
+            "from employees where username = ?";
+    private static final String AUTHORITIES_BY_USERNAME_QUERY = "select username, authorities.name " +
+            "from authorities inner join employees_authorities on authorities.id = employees_authorities.authority_id " +
+            "inner join employees on employees_authorities.employee_id = employees.id where username = ?";
+
+    private final DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .anyRequest().permitAll();
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .permitAll()
+                    .and()
+                .logout()
+                    .permitAll()
+                    .and()
+                .csrf()
+                    .disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                    .dataSource(dataSource)
+                    .usersByUsernameQuery(USERS_BY_USERNAME_QUERY)
+                    .authoritiesByUsernameQuery(AUTHORITIES_BY_USERNAME_QUERY)
+                    .passwordEncoder(passwordEncoder());
     }
 
     @Bean
@@ -34,5 +66,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Pbkdf2PasswordEncoder("", 1000, 160);
     }
 }
