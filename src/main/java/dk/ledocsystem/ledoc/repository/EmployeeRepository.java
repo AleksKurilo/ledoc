31864bb1a42ba72a07ebd480a1cf4fbf9b4643c2
Anton Chertash <a.chertash@dvsts.com>
2018-07-25 12:53:19 +0300

    Added some crud methods

--- src/main/java/dk/ledocsystem/ledoc/repository/EmployeeRepository.java
+++ src/main/java/dk/ledocsystem/ledoc/repository/EmployeeRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
@@ -34,4 +35,58 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     @Modifying
     @Query(value = "delete from main.employees e where e.id in ?1", nativeQuery = true)
     void deleteByIdIn(Collection<Long> ids);
+
+    /**
+     * @return All {@link Employee} employees that are not archived
+     */
+    List<Employee> findAllByArchivedIsFalse();
+
+    /**
+     * @return All {@link Employee} employees that are archived
+     */
+    List<Employee> findAllByArchivedIsTrue();
+
+    /**
+     * @param customerId customerId
+     * @return All {@link Employee} eployees of current {@link dk.ledocsystem.ledoc.model.Customer} company
+     */
+    List<Employee> findAllByCustomer_Id(Long customerId);
+
+    /**
+     * @param customerId customerId
+     * @return All {@link Employee} not archived eployees of current {@link dk.ledocsystem.ledoc.model.Customer} company
+     */
+    List<Employee> findAllByCustomer_IdAndArchivedIsFalse(Long customerId);
+
+    /**
+     * @param customerId customerId
+     * @return All {@link Employee} archived in given {@link dk.ledocsystem.ledoc.model.Customer} company
+     */
+    List<Employee> findAllByCustomer_IdAndArchivedIsTrue(Long customerId);
+
+    /**
+     * @param locationID locationID
+     * @param includeArchived includeArchived if we want get archived eployees too
+     * @return All {@link Employee} eployees of given {@link dk.ledocsystem.ledoc.model.Location} location
+     *//*
+    List<Employee> allbyLocation(Long );
+
+    *//**
+     * @param locationID locationID
+     * @return All {@link Employee} archived eployees of given {@link dk.ledocsystem.ledoc.model.Location} location
+     *//*
+    List<Employee> allbyLocationarchived(Long );
+
+    *//**
+     * @param addressId addressId
+     * @param includeArchived includeArchived if we want get archived eployees too
+     * @return All {@link Employee} employees of given {@link dk.ledocsystem.ledoc.model.Address} address
+     *//*
+    List<Employee> findAllInAddress(Long );
+
+    *//**
+     * @param addressId addressId
+     * @return All {@link Employee} archived employees of given {@link dk.ledocsystem.ledoc.model.Address} address
+     *//*
+    List<Employee> findAllArchivedInAddress(Long );*/
 }
