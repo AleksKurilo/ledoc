9adeefd2c82041e819984ebec306691cbc9c40e1
AleksKurilo <akurilo@outlook.com>
2018-10-05 12:32:32 +0300

    RestExceptionHandler return json with nesting errors during validation

--- src/main/java/dk/ledocsystem/ledoc/exceptions/RestExceptionHandler.java
+++ src/main/java/dk/ledocsystem/ledoc/exceptions/RestExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -36,12 +36,39 @@ class RestExceptionHandler implements AsyncUncaughtExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, Locale locale) {
+    public ResponseEntity<Map<String, Map<String, String>>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, Locale locale) {
         BindingResult result = ex.getBindingResult();
-        Map<String, String> errorMap = result.getFieldErrors()
+
+        Map<String, String> errors = new HashMap<>();
+        result.getFieldErrors()
                 .stream()
-                .collect(Collectors.toMap(FieldError::getField, (error) -> messageSource.getMessage(error, locale)));
-        return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body(errorMap);
+                .forEach(fieldError -> {
+                    String key = fieldError.getField();
+                    if (errors.containsKey(key)) {
+                        String val = errors.get(key) + "," + messageSource.getMessage(fieldError, locale);
+                        errors.put(key, val);
+                    } else {
+                        errors.put(key, messageSource.getMessage(fieldError, locale));
+                    }
+                });
+
+        Map<String, Map<String, String>> jsonErrors = new HashMap<>();
+        errors.entrySet().stream()
+                .map(s -> {
+                    Map<String, String> nestedObject = new HashMap<>();
+                    if (s.getKey().contains(".")) {
+                        String[] keys = s.getKey().split("\\.");
+                        nestedObject.put(keys[1], s.getValue());
+                        jsonErrors.put(keys[0], nestedObject);
+                        return null;
+                    } else {
+                        nestedObject.put("", s.getValue());
+                        jsonErrors.put(s.getKey(), nestedObject);
+                        return null;
+                    }
+                }).collect(Collectors.toList());
+
+        return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body(jsonErrors);
     }
 
     @ExceptionHandler(AccessDeniedException.class)
@@ -68,9 +95,9 @@ class RestExceptionHandler implements AsyncUncaughtExceptionHandler {
                 .body(error);
     }
 
-	private HttpStatus resolveResponseStatus(Exception exception) {
-		ResponseStatus annotation = AnnotatedElementUtils.findMergedAnnotation(exception.getClass(), ResponseStatus.class);
-		return (annotation != null) ? annotation.value() : HttpStatus.INTERNAL_SERVER_ERROR;
-	}
+    private HttpStatus resolveResponseStatus(Exception exception) {
+        ResponseStatus annotation = AnnotatedElementUtils.findMergedAnnotation(exception.getClass(), ResponseStatus.class);
+        return (annotation != null) ? annotation.value() : HttpStatus.INTERNAL_SERVER_ERROR;
+    }
 
 }
--- src/main/java/dk/ledocsystem/ledoc/exceptions/RestResponse.java
+++ src/main/java/dk/ledocsystem/ledoc/exceptions/RestResponse.java
@@ -5,28 +5,14 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import java.util.List;
-import java.util.Map;
 
 public class RestResponse {
-
-//	@JsonProperty
-//	private String message;
 
 	@JsonProperty
 	private List<String> messages;
 
-	@JsonProperty
-	private Map<String, String> message;
-
 	public RestResponse(List<String> messages) {
 		this.messages = messages;
-	}
-
-	//	public RestResponse(String message) {
-//		this.message = message;
-//	}
-	public RestResponse(Map<String, String> message) {
-		this.message = message;
 	}
 
 	static String logMessageFrom(Throwable th) {
