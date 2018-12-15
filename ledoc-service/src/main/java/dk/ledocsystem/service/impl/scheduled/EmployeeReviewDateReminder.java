86a4371a5f66768e530c98554f57244db68fa32a
Anton Zaporozhchenko <anton.zaporozhchenko@chisw.com>
2018-12-15 12:02:59 +0200

    Update version to 0.2

--- ledoc-service/src/main/java/dk/ledocsystem/service/impl/scheduled/EmployeeReviewDateReminder.java
+++ ledoc-service/src/main/java/dk/ledocsystem/service/impl/scheduled/EmployeeReviewDateReminder.java
@@ -19,7 +19,7 @@ class EmployeeReviewDateReminder {
     private final EmployeeRepository employeeRepository;
     private final EmailNotificationRepository emailNotificationRepository;
 
-    @Scheduled(cron = "0 0 4 * * *") // every weekday at 4:00 am
+    @Scheduled(cron = "0 0 4 * * *") // every day at 4:00 am
     public void checkReviewDates() {
         for (Employee employee : employeeRepository.findAllForReview()) {
             checkReviewDate(employee.getDetails().getNextReviewDate(), employee);
