e47c9907bcc6225cb6daadb14fd96b4b7e93fbbc
tatiana_shikota <23211508>
2019-01-08 11:22:16 +0200

    LD4-98 data for frontend after login

--- ledoc-api/src/main/java/dk/ledocsystem/api/config/security/AuthenticationSuccessHandlerImpl.java
+++ ledoc-api/src/main/java/dk/ledocsystem/api/config/security/AuthenticationSuccessHandlerImpl.java
@@ -1,19 +1,42 @@
 package dk.ledocsystem.api.config.security;
 
+import dk.ledocsystem.data.model.employee.Employee;
+import dk.ledocsystem.data.repository.EmployeeRepository;
+import dk.ledocsystem.service.api.exceptions.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static dk.ledocsystem.service.impl.constant.ErrorMessageKey.EMPLOYEE_USERNAME_NOT_FOUND;
+
 @Service
 public class AuthenticationSuccessHandlerImpl implements AuthenticationSuccessHandler {
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        response.sendRedirect("/userdetails");
+        String username = authentication.getName();
+        Employee user = employeeRepository.findByUsername(username)
+                .orElseThrow(() -> new NotFoundException(EMPLOYEE_USERNAME_NOT_FOUND, username));
+        Cookie cookie = new Cookie("id", user.getId().toString());
+        cookie.setMaxAge(60 * 5); //Store cookie for 5 minutes
+        response.addCookie(cookie);
+
+        cookie = new Cookie("email", username);
+        cookie.setMaxAge(60 * 5); //Store cookie for 5 minutes
+        response.addCookie(cookie);
+
+        cookie = new Cookie("authorities", authentication.getAuthorities().toString());
+        cookie.setMaxAge(60 * 5); //Store cookie for 5 minutes
+        response.addCookie(cookie);
     }
 }
