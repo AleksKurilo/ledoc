ded15ba72fbb7168d072a2a2d31369fe344837ed
tatiana_shikota <23211508>
2019-01-11 14:08:43 +0200

    LD4-98 User details in token

--- ledoc-api/src/main/java/dk/ledocsystem/api/config/security/AuthenticationSuccessHandlerImpl.java
+++ ledoc-api/src/main/java/dk/ledocsystem/api/config/security/AuthenticationSuccessHandlerImpl.java
@@ -3,6 +3,9 @@ package dk.ledocsystem.api.config.security;
 import dk.ledocsystem.data.model.employee.Employee;
 import dk.ledocsystem.data.repository.EmployeeRepository;
 import dk.ledocsystem.service.api.exceptions.NotFoundException;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -13,8 +16,11 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static dk.ledocsystem.service.impl.constant.ErrorMessageKey.EMPLOYEE_USERNAME_NOT_FOUND;
+import static dk.ledocsystem.service.impl.constant.SecurityConstants.JWT_SECRET;
 
 @Service
 public class AuthenticationSuccessHandlerImpl implements AuthenticationSuccessHandler {
@@ -30,13 +36,20 @@ public class AuthenticationSuccessHandlerImpl implements AuthenticationSuccessHa
 
         response.setHeader("Access-Control-Allow-Credentials", "true");
 
-        Cookie cookie = new Cookie("id", user.getId().toString());
-        response.addCookie(cookie);
+        String domain = request.getServerName();
 
-        cookie = new Cookie("email", username);
-        response.addCookie(cookie);
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", user.getId());
+        claims.put("username", username);
+        claims.put("authorities", authentication.getAuthorities().toArray());
 
-        cookie = new Cookie("authorities", authentication.getAuthorities().toString());
+        JwtBuilder builder = Jwts.builder()
+                .setAudience("user")
+                .addClaims(claims)
+                .signWith(SignatureAlgorithm.HS512, JWT_SECRET.getBytes());
+
+        Cookie cookie = new Cookie("info", builder.compact());
+        cookie.setPath("/");
         response.addCookie(cookie);
     }
 }
